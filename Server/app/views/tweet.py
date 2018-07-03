f5f6211ee261a892f9b9606a70bd21cfc12b724d
JoMingyu <city7310@naver.com>
2018-07-03 10:21:40 +0900

    [DONE] GET /, GET /public-timeline API

--- Server/app/views/tweet.py
+++ Server/app/views/tweet.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, Response, g, request
+from flask import Blueprint, Response, abort, g, redirect, url_for, request
+from flask_jwt_extended import get_jwt_identity, jwt_optional
 from flask_restful import Api
 
 from app.models.tweet import TweetModel
@@ -19,3 +20,35 @@ class Tweet(BaseResource):
         TweetModel.insert(owner=g.user, message=payload['message']).execute()
 
         return Response('', 201)
+
+
+@api.resource('/')
+class Index(BaseResource):
+    @jwt_optional
+    def get(self):
+        jwt = get_jwt_identity()
+
+        if jwt:
+            # 로그인된 사용자
+            users = UserModel.select().where(UserModel.username == jwt)
+            if not users:
+                abort(401)
+            else:
+                user = users[0]
+
+                return [{
+                    'message': tweet.message,
+                    'timestamp': tweet.timestamp
+                } for tweet in TweetModel.select().where(TweetModel.owner == user)]
+        else:
+            return redirect(url_for('app.views.tweet.publictimeline'))
+
+
+@api.resource('/public-timeline')
+class PublicTimeline(BaseResource):
+    def get(self):
+        return [{
+            'owner': tweet.owner.username,
+            'message': tweet.message,
+            'timestamp': tweet.timestamp
+        } for tweet in TweetModel.select().limit(30)]
